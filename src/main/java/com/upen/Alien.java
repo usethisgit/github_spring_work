e8f26a156b610b1fbf3121bb25fb3388a61be560
upender <upenderurlana@gmail.com>
2025-06-09 22:38:27 +0530

    adding scopes ie singleton,prototype

--- src/main/java/com/upen/Alien.java
+++ src/main/java/com/upen/Alien.java
@@ -1,6 +1,10 @@
 package com.upen;
 
 public class Alien {
+    int age;
+    public Alien() {
+        System.out.println("Alien object created");
+    }
     public void code() {
         System.out.println("Alien is coding");
     }
--- src/main/java/com/upen/App.java
+++ src/main/java/com/upen/App.java
@@ -9,8 +9,11 @@ public class App
     {
         // the below line creates a spring container where objects are created
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Alien alien = context.getBean("alien", Alien.class);
+        Alien alien1 = context.getBean("alien", Alien.class);
+        Alien alien2 = context.getBean("alien", Alien.class);
+       alien1.age = 20;
+        System.out.println(alien1.age);
+        System.out.println(alien2.age);
 
-        alien.code();
     }
 }
